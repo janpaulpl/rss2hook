360fcc5833d59d8a8737780aadbdb4f9db42d5f3
jpVinnie <janpaul.ramos@protonmail.com>
2022-11-01 09:12:07 -0400

    test webhook

--- src/discord.py
+++ src/discord.py
@@ -1,4 +1,4 @@
 from discordwebhook import Discord
 
-discord = Discord(url="<web hook url>")
+discord = Discord(url="https://discord.com/api/webhooks/1036990907687374911/oQfMr2EloMMBt6AZ2wTRu__MoeQW1BgXIbYEfsyNZj9i--a5tONPnzqVhzDq3Ay6hgQe")
 discord.post(content="Hello, world.")
